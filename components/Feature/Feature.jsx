105253ec71061c6224304fae4a13ebc3cd6d5776
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2021-07-04 18:45:35 -0400

    :sparkles: add snowIce section

--- components/Feature/Feature.jsx
+++ components/Feature/Feature.jsx
@@ -1,4 +1,8 @@
-import { InboxIcon, SparklesIcon } from '@heroicons/react/outline';
+import {
+  InboxIcon,
+  SparklesIcon,
+  ClipboardListIcon,
+} from '@heroicons/react/outline';
 
 const featureContent = {
   sectionTitle: `Call us. Then relax and sit back.`,
@@ -12,6 +16,12 @@ const featureContent = {
     'New exterior deck construction by the Stevens Services team.',
   constructionTitle: 'No job is too big or small',
   constructionDescription: `We've seen it all and have the expertise to handle anything you can throw our way. We work with individuals, small businesses, large enterprises, and churches throughout the Grerater Kansas City area.`,
+  snowIceTitle: 'Scheduled and professional',
+  snowIceDescription:
+    'Snow removal, pre-treatment for ice storms, during-storm ice treatment, and post-storm cleanup. We are tough enough for anything mother nature can throw our way.',
+  snowIceSrc: '/images/IMG_2411A_yctcla.jpg',
+  snowIceAlt:
+    'Our team has the tools, resources and experience to get handle any storm for any property.',
   testimonialSrc: '',
   testimonialAlt: '',
   testimonialName: 'Marcia Hill, Digital Marketing Manager',
@@ -125,6 +135,50 @@ export default function Feature() {
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
                 src={featureContent?.constructionSrc}
                 alt={featureContent?.constructionAlt}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-24">
+        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+            <div>
+              <div>
+                <span
+                  id="snow-ice"
+                  className="h-12 w-12 rounded-md flex items-center justify-center bg-black"
+                >
+                  <ClipboardListIcon
+                    className="h-6 w-6 text-white"
+                    aria-hidden="true"
+                  />
+                </span>
+              </div>
+              <div className="mt-6">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                  {featureContent?.snowIceTitle}
+                </h2>
+                <p className="mt-4 text-lg text-gray-500">
+                  {featureContent?.snowIceDescription}
+                </p>
+                <div className="mt-6">
+                  <a
+                    href={featureContent?.sectionButtonSrc}
+                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-black hover:text-black hover:bg-gray-300"
+                  >
+                    {featureContent?.sectionButtonText}
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
+            <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+              <img
+                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
+                src={featureContent?.snowIceSrc}
+                alt={featureContent?.snowIceAlt}
               />
             </div>
           </div>
