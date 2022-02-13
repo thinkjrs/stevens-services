626ecd8a36a1d393d103543b6eeaf996991c4b0d
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-02-12 20:15:15 -0600

    :recycle: refactor to include properly extracted Carousel component

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,35 +8,13 @@ import {
   QuestionMarkCircleIcon,
   XIcon,
 } from '@heroicons/react/outline';
-import { Rerousel } from 'rerousel';
 import { ChevronDownIcon } from '@heroicons/react/solid';
 import Feature from '../components/Feature/Feature';
 import About from '../components/About/About';
 import Footer from '../components/Footer/Footer';
 import Logo from '../components/Logo/Logo.jsx';
+import Carousel from '../components/Carousel/Carousel';
 
-const workItems = [
-  {
-    src: 'https://res.cloudinary.com/thinkjrs-dev/image/upload/c_scale,w_1920/v1644717022/stevens-services/20200629_161041_resized_phijb9.jpg',
-    alt: 'A new deck fortified and cleanly treated by the experts at Stevens Services.',
-  },
-  {
-    src: 'https://res.cloudinary.com/thinkjrs-dev/image/upload/v1644717022/stevens-services/20200629_160502_resized_nucuoo.jpg',
-    alt: 'Happy clients Karen & Cindy celebrate their new deck and landscaping!',
-  },
-  {
-    src: 'https://res.cloudinary.com/thinkjrs-dev/image/upload/c_scale,w_1980/v1644717022/stevens-services/20200629_161036_resized_hcfqas.jpg',
-    alt: "Karen & Cindy's landscaping by Stevens Services.",
-  },
-  {
-    src: 'https://res.cloudinary.com/thinkjrs-dev/image/upload/c_scale,w_1980/v1644717294/stevens-services/20200520_095233_aalzqs.jpg',
-    alt: 'A new deck and railing by the experts at Stevens Services.',
-  },
-  {
-    src: 'https://res.cloudinary.com/thinkjrs-dev/image/upload/c_scale,w_1980/v1644717298/stevens-services/20200520_095220_fwecfs.jpg',
-    alt: 'A new deck stairway by the best at Stevens Services.',
-  },
-];
 const heroContent = {
   heroTitle: 'Stevens Services',
   heroSubtitle: 'Simple. Professional. Turn-key.',
@@ -75,18 +53,8 @@ const navigation = [];
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
-function CallNowButton({ buttonText, buttonHref }) {
-  return (
-    <a
-      href={buttonHref}
-      className="my-8 flex uppercase items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-lg shadow-sm text-white bg-black shadow-sm hover:bg-gray-100 hover:text-black sm:px-8"
-    >
-      {buttonText}
-    </a>
-  );
-}
+
 export default function Example() {
-  const workItem = useRef(null);
   return (
     <div className="min-h-screen bg-white">
       <header>
@@ -318,35 +286,7 @@ export default function Example() {
         {/* More main page content here... */}
         <Feature />
         <About />
-        <div className="text-center">
-          <div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-20">
-            {' '}
-            <h1 className="text-3xl font-extrabold sm:text-4xl">
-              We pride ourselves on <span className="italic">quality</span> work
-            </h1>
-            <div id="quality-work" className="px-4 md:px-64 py-8 rounded-lg">
-              <Rerousel itemRef={workItem}>
-                {workItems.map((props, index) => {
-                  return (
-                    <img
-                      className="rounded-xl"
-                      key={`${index}-work-item`}
-                      height="100%"
-                      width="800"
-                      src={props?.src}
-                      alt={props?.alt}
-                      ref={workItem}
-                    />
-                  );
-                })}
-              </Rerousel>
-              <CallNowButton
-                buttonText="Call now"
-                buttonHref="tel:8168247289"
-              />
-            </div>
-          </div>
-        </div>
+        <Carousel />
       </main>
       <Footer />
     </div>
